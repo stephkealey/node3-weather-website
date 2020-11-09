4db23a5b3017b4c12b522c5d917f402b4dfadcb6
Kealey, Stephanie <stephanie.kealey@garmin.com>
2020-11-09 15:41:32 -0600

    add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            console.log(body.daily.data[0])
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
